ccf760714a984e7dda1038e9b0854df5243093d2
shanechern <45502177+shanechern@users.noreply.github.com>
2022-09-03 17:18:48 -0300

    Create intro.js

---
+++ intro.js
@@ -0,0 +1,14 @@
+document.write("<!-- Header -->");
+document.write("");
+document.write("<h1>Shane Chern<\/h1>");
+document.write("<p>Xiaohang Chen [陈小航]<\/p>");
+document.write("<p>Killam Postdoctoral Fellow<br>Department of Mathematics and Statistics<br>Dalhousie University<\/p>");
+document.write("<h3><a href=\"https:\/\/shanechern.github.io\/\">Home<\/a><\/h3>");
+document.write("<h3><a href=\"https:\/\/shanechern.github.io\/cv\/CV.pdf\">CV<\/a><\/h3>  ");
+document.write("<h3><a href=\"https:\/\/shanechern.github.io\/publications.html\">Publications<\/a><\/h3> ");
+document.write("<h3><a href=\"https:\/\/shanechern.github.io\/teaching.html\">Teaching<\/a><\/h3> ");
+document.write("<h3><a href=\"https:\/\/shanechern.github.io\/more.html\">More...<\/a><\/h3> ");
+document.write("<br>");
+document.write("");
+document.write("<p><b>Contact:<\/b><br><i>Office<\/i>: Room 254, Chase Building, Dalhousie University<br><i>Email<\/i>: <a href=\"mailto:chenxiaohang92@gmail.com\">chenxiaohang92@gmail.com<\/a><\/p>");
+document.write("");
