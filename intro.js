b57e1caff0aade216b06a148fcad1695762cb753
shanechern <45502177+shanechern@users.noreply.github.com>
2024-09-13 03:03:34 +0200

    Update intro.js

--- intro.js
+++ intro.js
@@ -2,7 +2,7 @@ document.write("<!-- Header -->");
 document.write("");
 document.write("<h1>Shane Chern<\/h1>");
 document.write("<p>Xiaohang Chen [陈小航]<\/p>");
-document.write("<p>Killam Postdoctoral Fellow<br>Department of Mathematics and Statistics<br>Dalhousie University<\/p>");
+document.write("<p>Postdoctoral Researcher<br>Faculty of Mathematics<br>University of Vienna<\/p>");
 document.write("<h3><a href=\"https:\/\/shanechern.github.io\/\">Home<\/a><\/h3>");
 document.write("<h3><a href=\"https:\/\/shanechern.github.io\/cv\/CV.pdf\">CV<\/a><\/h3>  ");
 document.write("<h3><a href=\"https:\/\/shanechern.github.io\/publications.html\">Publications<\/a><\/h3> ");
@@ -10,5 +10,5 @@ document.write("<h3><a href=\"https:\/\/shanechern.github.io\/teaching.html\">Te
 document.write("<h3><a href=\"https:\/\/shanechern.github.io\/more.html\">More...<\/a><\/h3> ");
 document.write("<br>");
 document.write("");
-document.write("<p><b>Contact:<\/b><br><i>Office<\/i>: Room 254, Chase Building, Dalhousie University<br><i>Email<\/i>: <a href=\"mailto:chenxiaohang92@gmail.com\">chenxiaohang92@gmail.com<\/a><\/p>");
+document.write("<p><b>Contact:<\/b><br><i>Office<\/i>: Room 02.124, Oskar-Morgenstern-Platz 1, 1090 Wien, Austria<br><i>Email<\/i>: <a href=\"mailto:chenxiaohang92@gmail.com\">chenxiaohang92@gmail.com<\/a><\/p>");
 document.write("");
