39676b31e97010b1e80cb507ac632ec6865089d4
shanechern <45502177+shanechern@users.noreply.github.com>
2024-12-13 04:40:08 +0100

    Update intro.js

--- intro.js
+++ intro.js
@@ -10,5 +10,5 @@ document.write("<h3><a href=\"https:\/\/shanechern.github.io\/teaching.html\">Te
 document.write("<h3><a href=\"https:\/\/shanechern.github.io\/more.html\">More...<\/a><\/h3> ");
 document.write("<br>");
 document.write("");
-document.write("<p><b>Contact:<\/b><br><i>Office<\/i>: Room 02.124, Oskar-Morgenstern-Platz 1, 1090 Wien, Austria<br><i>Email<\/i>: <a href=\"mailto:chenxiaohang92@gmail.com\">chenxiaohang92@gmail.com<\/a><\/p>");
+document.write("<p><b>Contact:<\/b><br><i>Office<\/i>: Room O2.124, Oskar-Morgenstern-Platz 1, 1090 Wien, Austria<br><i>Email<\/i>: <a href=\"mailto:chenxiaohang92@gmail.com\">chenxiaohang92@gmail.com<\/a><\/p>");
 document.write("");
